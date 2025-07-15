6be32f3d3fc0327d63a71fca77260a4dd1ec1312
seyona511 <soulmatch63@gmail.com>
2025-07-14 21:45:16 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+
+// TODO: Substitua com as suas credenciais reais do Firebase
+var firebaseConfig = {
+    apiKey: "SUA_API_KEY",
+    authDomain: "SEU_DOMINIO.firebaseapp.com",
+    projectId: "SEU_ID",
+    storageBucket: "SEU_BUCKET.appspot.com",
+    messagingSenderId: "SEU_ID",
+    appId: "SEU_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
